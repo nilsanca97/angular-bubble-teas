adb966c90526c4e912c56e6e64a47db174b06086
nil.sanca97 <nil.sanca97@gmail.com>
2026-05-27 11:23:49 +0200

    add interfaces about my objects (from backend). For the moment, I only use User interfaces (for create register) and "BubbleTea" interface later for show bubble-teas in home component.

---
+++ src/app/models/interfaces.ts
@@ -0,0 +1,14 @@
+export interface BubbleTea {
+  id: number;
+  name: string;
+  temperature: number;
+  price: number;
+  active: boolean;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
