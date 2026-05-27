725fd75c7e650910194a65e9c03aedf2076b4118
nil.sanca97 <nil.sanca97@gmail.com>
2026-05-27 11:33:55 +0200

    Crea el FormGroup como una variable de clase, sin usar constructor. en el componente register.ts

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +7,30 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+
+export class Register {
+  protected registerForm = new FormGroup({
+    name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+    confirmPassword: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+  });
+
+  protected onRegisterClick(): void {
+    const { name, email, password, confirmPassword } = this.registerForm.value;
+    console.log({ name, email, password, confirmPassword });
+    // aquí puedes validar la confirmación de password o hacer otra acción
+  }
+}
