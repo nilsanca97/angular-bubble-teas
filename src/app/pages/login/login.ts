7aa6e0794c9f83fa56bebe63ee9eec0e9dbb37e4
nil.sanca97 <nil.sanca97@gmail.com>
2026-05-27 12:58:32 +0200

    crear la logica del componente login (a través de login.ts). Formulario como variable de clase (siguiendo el mismo patron que register). Usando REACTIVEFORMSMODULE

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../models/interfaces';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  protected loginForm = new FormGroup({
+    email: new FormControl<User['email']>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl<User['password']>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+  });
+
+  protected onLoginClick(): void {
+    const formValue = this.loginForm.value;
+    console.log('Login:', formValue);
+    // agregar la lógica de autenticación (en la segunda fase)
+  }
+}
