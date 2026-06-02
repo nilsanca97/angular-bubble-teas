410772dc95e23e37e089ce7ca42e3791a72648f9
nilsanchezuoc <150086223+nilsanchezuoc@users.noreply.github.com>
2026-06-02 23:08:59 +0200

    feat: renderizar la lista (array) de bubbleteas en la pagina home.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core'; // → la "interfaz" que nos da el momento ngOnInit. signal → caja reactiva que avisa a la pantalla.
+import { BubbleTea } from '../../models/interfaces'; // → la interface (BubbleTea), para tipar la libreta.
+import { BubbleTeaService } from '../../services/bubble-tea.service';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+
+
+// implements OnInit -> es una promesa: "esta clase tendrá un método ngOnInit"
+export class Home implements OnInit {
+
+
+  // PASO 2: la "libreta" reactiva donde guardaremos los bubble teas. Empieza vacía.
+  // Al ser una signal, cuando cambie avisará sola a la pantalla para repintar.
+  bubbleTeas = signal<BubbleTea[]>([]);
+
+  // PASO 1: inyectamos el service (el "teléfono" a la cocina/backend)
+  constructor(private bubbleTeaService: BubbleTeaService) {}
+
+  // PASO 3: ngOnInit se ejecuta cuando la página se carga. Aquí arrancamos el trabajo.
+  ngOnInit(): void {
+    // PASO 4: pedimos los datos y nos suscribimos.
+    // Cuando lleguen, los guardamos en la libreta.
+    this.bubbleTeaService.getAll().subscribe((datos) => {
+      // .set() mete los datos en la signal Y avisa a la pantalla para que repinte
+      this.bubbleTeas.set(datos);
+    });
+  }
+}
